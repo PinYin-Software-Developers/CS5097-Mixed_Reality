f957cf1990930fb2f8b94967250ff433225d3c84
Bhanu <pachipala.b@northeastern.edu>
2025-03-25 15:28:56 -0400

    Updated code with gun and trigger working

--- Assets/RayGun.cs
+++ Assets/RayGun.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class RayGun : MonoBehaviour
 {
+    public LayerMask layerMask;
     public OVRInput.RawButton shootingButton;
     public LineRenderer linePrefab;
     public Transform shootingPoint;
     public float maxLineDistance = 5;
+    public float lineShowTimer = 0.3f;
+    public AudioSource source;
+    public AudioClip shootingAudioClip;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,20 +31,31 @@ public class RayGun : MonoBehaviour
 
     public void Shoot()
     {
-        // // LineRenderer line = Instantiate(linePrefab);
-        // GameObject block = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        // block.transform.position = new Vector3(0, 0, 0);
-
-        // Create a new cube
-        GameObject block = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        source.PlayOneShot(shootingAudioClip);
         
-        // Randomize the direction
-        Vector3 randomDirection = Random.insideUnitSphere.normalized * maxLineDistance;
-        randomDirection += shootingPoint.position; // Start from the shooting point
+        Ray ray = new Ray(shootingPoint.position, shootingPoint.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layerMask);
 
-        // Set the block's position
-        block.transform.position = randomDirection;
+        Vector3 endPoint = Vector3.zero;
+
+        if(hasHit)
+        {
+            endPoint = hit.point;
+        }
+        else
+        {
+            endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        }
+
+
+        LineRenderer line = Instantiate(linePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0, shootingPoint.position);
+
+
+        // Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        line.SetPosition(1, endPoint);
+
+        Destroy(line.gameObject, lineShowTimer);
     }
 }
-
-
